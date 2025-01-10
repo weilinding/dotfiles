95eb9a2e1eda01805559da0021b38fe3c97a872e
weilinding <lynn_ding@yahoo.com>
2025-01-10 10:25:33 -0800

    bk 2025 jan

---
+++ .ssh/port-number.sh
@@ -0,0 +1,6 @@
+ssh -f -N -L 3389:localhost:3389 MCWS5
+ssh -f -N -L 5900:localhost:5900 MCS1
+ssh -f -N -L 5901:localhost:5900 MCM1
+ssh -f -N -L 5902:localhost:5900 MCM2
+ssh -f -N -L 5903:localhost:5900 MCM3
+ssh -f -N -L 5904:localhost:5900 MCM4
